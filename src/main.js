85a02299301a775f3d3c081796b7337c5c7153b9
1201tengyang <tengyangcc@163.com>
2017-11-29 10:43:33 +0800

    update

--- src/main.js
+++ src/main.js
@@ -3,11 +3,14 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
+import VueScroller from 'vue-scroller'
 
 import './mock/mockServer'
 import './filters'
 import '../static/reset.css'
 import './common/stylus/fonts.styl'
+
+Vue.use(VueScroller)
 
 new Vue({
   el: '#app',
